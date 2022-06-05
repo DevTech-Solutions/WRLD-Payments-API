f7c1f4fa6ae4fe852d67af6c074d7c0fa66d55ab
LBuke <lbingham1997@gmail.com>
2022-06-05 19:43:56 +0100

    Only allow players to run wallet gui command

--- src/main/java/com/nftworlds/wallet/commands/WalletGUICommand.java
+++ src/main/java/com/nftworlds/wallet/commands/WalletGUICommand.java
@@ -8,8 +8,13 @@ import org.bukkit.entity.Player;
 import org.jetbrains.annotations.NotNull;
 
 public class WalletGUICommand implements CommandExecutor {
+
+    @Override
     public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String[] args) {
-        WalletGUI.menu.open((Player) sender);
+        if (!(sender instanceof Player player))
+            return true;
+
+        WalletGUI.menu.open(player);
         return true;
     }
 
